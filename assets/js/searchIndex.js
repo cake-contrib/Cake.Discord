d5034fec85053510b3a5d825e5565d44d8be2ee4
Gary Ewan Park <gep13@gep13.co.uk>
2019-05-04 13:33:34 +0000

    AppVeyor Publish: 5a94b519094c176aa3d84224a28523092e6929c2 Merge branch 'release/0.2.0'
    * release/0.2.0:
      (build) Ensure GitVersion is always run
      (maint) Updated nuspec metadata
      (build) Pin to Cake and Cake.Recipe
      (GH-5) Cake.Common reference should be private
      (GH-5) Upgrade Cake.Common reference to 0.33.0
      (GH-5) Cake.Core reference should be private
      (GH-5) Upgrade Cake.Core reference to 0.33.0
      (GH-5) Fix iconUrl
      (build) Update to latest bootstrappers
      (build) Update appveyor.yml file
      (doc) Added note about pinning version number
      (build) Test actual release of Cake.Discord

--- assets/js/searchIndex.js
+++ assets/js/searchIndex.js
@@ -39,34 +39,6 @@ var searchModule = function() {
     a(
         {
             id:0,
-            title:"DiscordChatMessageResult",
-            content:"DiscordChatMessageResult",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/Cake.Discord/api/Cake.Discord.Chat/DiscordChatMessageResult',
-            title:"DiscordChatMessageResult",
-            description:""
-        }
-    );
-    a(
-        {
-            id:1,
-            title:"DiscordChatMessageSettings",
-            content:"DiscordChatMessageSettings",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/Cake.Discord/api/Cake.Discord.Chat/DiscordChatMessageSettings',
-            title:"DiscordChatMessageSettings",
-            description:""
-        }
-    );
-    a(
-        {
-            id:2,
             title:"DiscordProvider",
             content:"DiscordProvider",
             description:'',
@@ -80,21 +52,7 @@ var searchModule = function() {
     );
     a(
         {
-            id:3,
-            title:"DiscordAliases",
-            content:"DiscordAliases",
-            description:'',
-            tags:''
-        },
-        {
-            url:'/Cake.Discord/api/Cake.Discord/DiscordAliases',
-            title:"DiscordAliases",
-            description:""
-        }
-    );
-    a(
-        {
-            id:4,
+            id:1,
             title:"DiscordChatProvider",
             content:"DiscordChatProvider",
             description:'',
@@ -103,6 +61,48 @@ var searchModule = function() {
         {
             url:'/Cake.Discord/api/Cake.Discord.Chat/DiscordChatProvider',
             title:"DiscordChatProvider",
+            description:""
+        }
+    );
+    a(
+        {
+            id:2,
+            title:"DiscordChatMessageSettings",
+            content:"DiscordChatMessageSettings",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.Discord/api/Cake.Discord.Chat/DiscordChatMessageSettings',
+            title:"DiscordChatMessageSettings",
+            description:""
+        }
+    );
+    a(
+        {
+            id:3,
+            title:"DiscordChatMessageResult",
+            content:"DiscordChatMessageResult",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.Discord/api/Cake.Discord.Chat/DiscordChatMessageResult',
+            title:"DiscordChatMessageResult",
+            description:""
+        }
+    );
+    a(
+        {
+            id:4,
+            title:"DiscordAliases",
+            content:"DiscordAliases",
+            description:'',
+            tags:''
+        },
+        {
+            url:'/Cake.Discord/api/Cake.Discord/DiscordAliases',
+            title:"DiscordAliases",
             description:""
         }
     );
